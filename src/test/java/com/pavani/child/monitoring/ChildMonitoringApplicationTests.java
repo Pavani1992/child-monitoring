8b09acfde11577b68bcf80a910c15c0c22a26ee7
Rakesh Tatabhatla <rakeshtatabhatla@Rakeshs-MacBook-Air.local>
2022-08-30 22:10:53 -0500

    initial commit

---
+++ src/test/java/com/pavani/child/monitoring/ChildMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavani.child.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
